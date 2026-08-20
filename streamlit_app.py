d5c7ef4064a435c3f0a19a8f111bb114c8c85ccd
aexxlo54 <142586586+aexxlo54@users.noreply.github.com>
2026-08-20 13:00:50 +0800

    Modify import and comment out dataframe display
    Updated import statement and commented out dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-# from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.markdown("<h1>🥤 Customize Your Smoothie! 🥤</h1>", unsafe_allow_html=True)
@@ -26,8 +26,8 @@ st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:'
@@ -43,6 +43,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
+
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
             st.subheader(fruit_chosen + ' Nutrition Information')
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

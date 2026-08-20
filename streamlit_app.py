97ab975a71061850b1a7e612463eba076b13cd15
aexxlo54 <142586586+aexxlo54@users.noreply.github.com>
2026-08-20 13:14:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
-import requests
-import pandas as pd
-# from snowflake.snowpark.functions import col
+import requests 
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.markdown("<h1>🥤 Customize Your Smoothie! 🥤</h1>", unsafe_allow_html=True)
@@ -19,14 +18,10 @@ name_on_order = st.text_input ('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
-my_dataframe = cnx.query("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.public.fruit_options")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
-
-# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df = my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 Ingredients:'
@@ -42,12 +37,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
-
-            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-        
             st.subheader(fruit_chosen + ' Nutrition Information')
-            smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
@@ -60,7 +51,6 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
-        cnx.query(my_insert_stmt)
-        
-        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
+        session.sql(my_insert_stmt).collect()
 
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
